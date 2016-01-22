ec34b9f3f6f07049c856e1675a46ca51bb30ded7
Arnold Cano <arnoldcano@yahoo.com>
2016-01-21 19:38:54 -0600

    Set header earlier in case of errors

--- runner/handlers.go
+++ runner/handlers.go
@@ -10,6 +10,8 @@ import (
 func RunHandler(w http.ResponseWriter, r *http.Request) {
 	var c http.Client
 
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
 	log.Printf("Received request from %s", r.RemoteAddr)
 	r2, err := http.NewRequest("POST", "http://usul:8080/run", r.Body)
 	r2.Header.Set("Content-Type", "application/json; charset=UTF-8")
@@ -22,7 +24,6 @@ func RunHandler(w http.ResponseWriter, r *http.Request) {
 	defer w2.Body.Close()
 	log.Printf("Received response from %s", r2.Host)
 
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	_, err = io.Copy(w, w2.Body)
 	if err != nil {
 		writeError(w, err)
